52b5fcd76e5b13a94f3534795a8313d210246037
byels <byelsey1@gmail.com>
2014-11-17 22:21:43 -0500

    prototypes for TBD functions, tweak comment alignment

--- ngx_http_response_codes_filter.c
+++ ngx_http_response_codes_filter.c
@@ -9,17 +9,26 @@ typedef struct {
 
 
 static ngx_int_t ngx_http_response_codes_filter_init (ngx_conf_t*);
+static ngx_int_t ngx_http_response_codes_header_filter(ngx_http_request_t*);
+static char * ngx_http_response_codes_init_main_conf(ngx_conf_t*, void*);
+static void * ngx_http_response_codes_create_main_conf(ngx_conf_t*);
 
 
-static ngx_http_module_t  ngx_http_log_request_speed_filter_ctx = {
-  NULL,                                     /* preconfiguration */
-  ngx_http_response_codes_filter_init,      /* postconfiguration */
-  ngx_http_response_codes_create_main_conf, /* create main configuration */
-  ngx_http_response_codes_init_main_conf,   /* init main configuration */
-  NULL,                                     /* create server configuration */
-  NULL,                                     /* merge server configuration */
-  NULL,                                     /* create location configuration */
-  NULL                                      /* merge location configuration */
+static ngx_http_output_header_filter_pt ngx_http_next_header_filter;
+
+
+static ngx_command_t ngx_http_response_codes_filter_commands[] = {};
+
+
+static ngx_http_module_t ngx_http_log_request_speed_filter_ctx = {
+  NULL,                                              /* preconfiguration */
+  ngx_http_response_codes_filter_init,               /* postconfiguration */
+  ngx_http_response_codes_create_main_conf,          /* create main configuration */
+  ngx_http_response_codes_init_main_conf,            /* init main configuration */
+  NULL,                                              /* create server configuration */
+  NULL,                                              /* merge server configuration */
+  NULL,                                              /* create location configuration */
+  NULL                                               /* merge location configuration */
 };
 
 
